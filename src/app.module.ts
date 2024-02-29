a4ef9d98a0c51cea62c168b7d6ad0e9ba419fe81
Larissa Dalimar <larissadalimar@dcc.ufrj.br>
2024-02-29 18:16:23 -0300

    test solutions to cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,6 @@ import { DatabaseModule } from './database/database.module';
 import { ContentModule } from './content/content.module';
 import { ConfigModule } from '@nestjs/config';
 import { TagModule } from './tag/tag.module';
-import { CorsMiddleware } from './cors.middleware';
 
 @Module({
   imports: [AuthModule, UsersModule, DatabaseModule, ContentModule, ConfigModule.forRoot(), TagModule],
@@ -15,8 +14,4 @@ import { CorsMiddleware } from './cors.middleware';
   providers: [AppService],
 })
 
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(CorsMiddleware).forRoutes('*'); // Apply CorsMiddleware to all routes
-  }
-}
+export class AppModule {}
