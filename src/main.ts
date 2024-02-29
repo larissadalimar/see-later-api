ac711ca344dede764bf073754f50750d4df78411
Larissa Dalimar <larissadalimar@dcc.ufrj.br>
2024-02-29 18:24:57 -0300

    test with domain

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
-  
+  app.enableCors({
+    origin: 'https://lulugonn.github.io'
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
