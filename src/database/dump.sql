9f8983ab1d5ddb4d51e390b7ff1f85fcfad3ace1
Larissa Dalimar <larissadalimar@dcc.ufrj.br>
2024-04-18 12:27:45 -0300

    create nem database

--- src/database/dump.sql
+++ src/database/dump.sql
@@ -2,7 +2,7 @@ CREATE TABLE users(
     id serial primary key,
     name VARCHAR(30) not null,
     email varchar(100) not null,
-    password varchar(50) not null
+    password varchar(100) not null
 );
 
 CREATE TABLE contents(
@@ -10,6 +10,10 @@ CREATE TABLE contents(
     title VARCHAR(30) not null,
     url varchar(100) not null,
     notes text,
+    "type" VARCHAR(30) not null,
+    seen BOOLEAN DEFAULT FALSE,
+    consume_date TIMESTAMP,
+    favorite BOOLEAN DEFAULT false,
     "userId" integer references users(id),
     "createdAt" TIMESTAMP DEFAULT now() NOT NULL,
     "updatedAt" TIMESTAMP DEFAULT now() NOT NULL
@@ -26,5 +30,5 @@ CREATE TABLE categories(
 CREATE TABLE category_content(
     id serial primary key,
     content_id integer references contents(id),
-    category_id integer references categories(id),
+    category_id integer references categories(id)
 );
