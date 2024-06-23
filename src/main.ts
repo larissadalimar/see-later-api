498569aea132a7ec0001a53088c2957eab5414b4
Larissa Dalimar <larissadalimar@dcc.ufrj.br>
2024-06-23 14:41:46 -0300

    come back

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const corsOptions : CorsOptions = {
-    origin: 'https://lulugonn.github.io/see_later_app/', 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
-    preflightContinue: false,
-    optionsSuccessStatus: 204
-  }
-  app.enableCors(corsOptions);
+  app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
