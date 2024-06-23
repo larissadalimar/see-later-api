3295c4ffc476dbfc607a1e35f2e6d1d6868f9ee4
Larissa Dalimar <larissadalimar@dcc.ufrj.br>
2024-06-23 13:48:17 -0300

    try fix preflight 204 options

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: '*', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  });
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
